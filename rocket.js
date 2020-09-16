a8d71c62956fedf2e07f79f43d3f246db17a8122
Aissa Feldmann <aissaf@umich.edu>
2020-09-16 14:57:24 -0400

    added secondary engines

--- rocket.js
+++ rocket.js
@@ -1,10 +1,14 @@
 var Rocket = function (color, size) {
-    this.color = color;
-    this.size = size;
-  
-    this.launch = function () {
-      console.log('3, 2, 1, blastoff!');
-    }
-  }
-  
-  module.exports = Rocket;
+  this.color = color;
+  this.size = size;
+
+  this.launch = function () {
+    console.log('3, 2, 1, blastoff!');
+  };
+
+  this.engageSecondaryEngines = function () {
+    console.log('Secondary Engines Engaged!');
+  };
+};
+
+module.exports = Rocket;
